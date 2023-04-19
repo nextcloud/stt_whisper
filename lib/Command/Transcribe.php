9b2706a40eb76883f6f16be299bf9134219e064c
Marcel Klehr <mklehr@gmx.net>
2023-04-19 13:33:01 +0200

    Implement Transcribe command

---
+++ lib/Command/Transcribe.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Marcel Klehr <mklehr@gmx.net>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\SttWhisper\Command;
+
+use OCA\SttWhisper\Service\SpeechToTextService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Transcribe extends Command {
+	public function __construct(
+        private SpeechToTextService $stt
+    ) {
+		parent::__construct();
+	}
+
+	/**
+	 * Configure the command
+	 *
+	 * @return void
+	 */
+	protected function configure() {
+		$this->setName('stt_whisper:transcribe')
+			->setDescription('Transcribe a media file')
+			->addArgument('path');
+	}
+
+	/**
+	 * Execute the command
+	 *
+	 * @param InputInterface  $input
+	 * @param OutputInterface $output
+	 *
+	 * @return int
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output): int {
+		try {
+			$output->writeln($this->stt->transcribe($input->getArgument('path')));
+			return 0;
+		} catch(\RuntimeException $e) {
+			$output->writeln($e->getMessage());
+			return 1;
+		}
+	}
+}
