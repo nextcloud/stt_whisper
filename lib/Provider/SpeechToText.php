ac34cbf99791167cb5569c5ea6bb2c3f69d2419a
Marcel Klehr <mklehr@gmx.net>
2023-04-28 12:54:16 +0200

    fix(SpeechToText): Make name translatable, fix log messages and use constructor property prmotion

--- lib/Provider/SpeechToText.php
+++ lib/Provider/SpeechToText.php
@@ -8,26 +8,20 @@ namespace OCA\SttWhisper\Provider;
 use OCA\SttWhisper\Service\SpeechToTextService;
 use OCP\Files\File;
 use OCP\Files\NotFoundException;
-use OCP\ICacheFactory;
 use OCP\IL10N;
 use OCP\SpeechToText\ISpeechToTextProvider;
 use Psr\Log\LoggerInterface;
 
 class SpeechToText implements ISpeechToTextProvider {
-	private SpeechToTextService $transcriber;
-
-	private LoggerInterface $logger;
-	private IL10N $l;
-
-	public function __construct(ICacheFactory $cacheFactory, SpeechToTextService $transcriber, LoggerInterface $logger, IL10N $l) {
-		$this->cacheFactory = $cacheFactory;
-		$this->transcriber = $transcriber;
-		$this->logger = $logger;
-		$this->l = $l;
+	public function __construct(
+		private SpeechToTextService $transcriber,
+		private LoggerInterface $logger,
+		private IL10N $l
+	) {
 	}
 
 	public function getName(): string {
-		return 'Whisper Speech-To-Text';
+		return $this->l->t('Whisper Speech-To-Text');
 	}
 
 	public function transcribeFile(File $file): string {
@@ -35,8 +29,8 @@ class SpeechToText implements ISpeechToTextProvider {
 			$filePath = $file->getStorage()->getLocalFile($file->getPath());
 			return $this->transcriber->transcribe($filePath);
 		} catch(\RuntimeException|NotFoundException $e) {
-			$this->logger->warning('Translation failed with: ' . $e->getMessage(), ['exception' => $e]);
-			throw new \RuntimeException('Translation failed with: ' . $e->getMessage());
+			$this->logger->warning('Transcription failed with: ' . $e->getMessage(), ['exception' => $e]);
+			throw new \RuntimeException('Transcription failed with: ' . $e->getMessage());
 		}
 	}
 }
